4362d406dd2e27d2b6ffc6188bae3aa49d8d0c58
theophy <theo4u@ymail.com>
2017-03-16 23:49:37 +0100

    done with artist

--- scripts/directives/artists.directive.js
+++ scripts/directives/artists.directive.js
@@ -2,3 +2,28 @@
  * Created by SQ04 on 3/16/2017.
  * Theophilus Omoregbee <theo4u@ymail.com>
  */
+
+(function () {
+    'use strict';
+    angular.module("MusicSearch")
+        .directive("artists", Artists);
+
+    function Artists() {
+        return {
+            restrict: 'E',
+            scope: {
+                data: "=",
+                currentPage: "="
+            },
+            templateUrl: "views/directives/artists.directive.html",
+            controller: ArtistsController
+        }
+    }
+
+    ArtistsController.$inject = ["$scope"];
+    function ArtistsController($scope) {
+        $scope.currentPage = $scope.data.offset + $scope.data.limit;
+        console.log("artist directive", $scope.data);
+    }
+
+})();
