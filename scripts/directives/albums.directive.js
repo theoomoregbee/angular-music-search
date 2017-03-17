9120f44e0fe70f97104170811db07f4ae83633de
Theophilus Omoregbee <theo4u@ymail.com>
2017-03-17 16:33:50 +0100

    pagination done for album

--- scripts/directives/albums.directive.js
+++ scripts/directives/albums.directive.js
@@ -20,10 +20,56 @@
         }
     }
 
-    AlbumsController.$inject = ["$scope"];
-    function AlbumsController($scope) {
+    AlbumsController.$inject = ["$scope", "Spotify"];
+    function AlbumsController($scope, Spotify) {
         $scope.currentPage = $scope.data.offset + $scope.data.items.length;
         console.log("album directive", $scope.data);
+
+        $scope.paginator = {
+            total: $scope.data.total,
+            maxSize: 9,
+            currentPage: 1,
+            pageChanged: pageChanged
+        };
+        $scope.fetching = false; //used to hold when the pagination is process
+
+
+        /**
+         * this is used to trigger our reload function when the page changes
+         */
+        function pageChanged() {
+            console.log('Page changed to: ' + $scope.paginator.currentPage);
+
+            /**
+             * incase the user changes the page as we are fetching it should de reference the scope binding with the
+             * view
+             * @type {*}
+             */
+            var page = angular.copy($scope.paginator.currentPage);
+
+            $scope.fetching = true;
+            var offset = $scope.data.limit * page; //calculate pagination
+            var search_query = $scope.data.search;
+
+            Spotify.search(["album"], search_query, offset, $scope.data.limit).then(function (success) {
+                $scope.fetching = false;
+                console.log(success);
+
+                $scope.data = success.data.albums;
+                $scope.data.search = search_query;
+
+                //update our paginate directive
+                $scope.paginator.currentPage = page;
+
+                //update our outer count of how many result fetched of total
+                $scope.currentPage = success.data.albums.offset;
+
+            }, function (failure) {
+                console.error(failure);
+                $scope.fetching = false;
+            });
+
+        }
     }
 
 })();
